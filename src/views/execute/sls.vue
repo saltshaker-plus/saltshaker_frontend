8ccba7acf088c0e9561ea422c00afc0211d94cfd
yongpeng1 <yongpeng1@staff.sina.com.cn>
2018-04-15 19:39:05 +0800

    Added sls page

--- src/views/execute/sls.vue
+++ src/views/execute/sls.vue
@@ -9,7 +9,7 @@
             <Col span="24">
                 <Card>
                     <Row :gutter="10">
-                        <Select style="width:200px">
+                        <Select style="width:200px" v-model="productId">
                             <Option v-for="item in productData" :value="item.id" :key="item.id">{{ item.name }}</Option>
                         </Select>
                         <div style="float: right;" >
@@ -27,12 +27,12 @@
                                     选择分支
                                 </p>
                                 <div style="margin-bottom: -10px;">
-                                <Select>
-                                    <Option v-for="item in branchData" :value="item.id" :key="item.id">{{ item.name }}</Option>
+                                <Select v-model="branchName">
+                                    <Option v-for="item in branchData" :value="item" :key="item">{{ item }}</Option>
                                 </Select>
                                 </div>
                                 <br>
-                                <Tree :data="data3" :load-data="loadData"></Tree>
+                                <Tree :data="fileTree" :load-data="loadData"></Tree>
                             </Card>
                         </Col>
                         <Col span="18">
@@ -55,43 +55,87 @@
     export default {
         data () {
             return {
-                productData: this.product(),
-                data3: [
-                    {
-                        title: 'parent',
-                        loading: false,
-                        children: []
-                    }
-                ]
+                productData: this.productList(),
+                productId: '',
+                productStateProject: '',
+                branchData: [],
+                branchName: '',
+                fileTreeDate: [],
+                fileTree: []
             };
         },
+        watch: {
+            // 监控产品线变化
+            productId () {
+                this.branch();
+            },
+            branchName () {
+                this.file();
+            }
+        },
         methods: {
-            product () {
+            productList () {
                 this.axios.defaults.withCredentials = true; // 带着cookie
                 this.axios.get('http://192.168.44.128:5000/saltshaker/api/v1.0/product').then(
                     res => {
                         if (res.data['status'] === true) {
                             this.productData = res.data['products']['product'];
+                            this.productId = this.productData[0].id;
+                            this.productStateProject = this.productData[0].state_project;
                         } else {
                             this.nerror('Get Product Failure', res.data['message']);
                         }
                     },
                     err => {
-                        this.nerror('Get Product Failure', err);
+                        let errInfo = '';
+                        try {
+                            errInfo = err.response.data['message'];
+                        } catch (error) {
+                            errInfo = err;
+                        }
+                        this.nerror('Get Product Failure', errInfo);
                     });
             },
             branch () {
                 this.axios.defaults.withCredentials = true; // 带着cookie
-                this.axios.get('http://192.168.44.128:5000/saltshaker/api/v1.0/gitlab/branch?').then(
+                this.axios.get('http://192.168.44.128:5000/saltshaker/api/v1.0/gitlab/branch?product_id=' + this.productId + '&project_type=state_project').then(
                     res => {
                         if (res.data['status'] === true) {
                             this.branchData = res.data['branchs']['branch'];
+                            this.branchName = this.branchData[0];
                         } else {
                             this.nerror('Get Branch Failure', res.data['message']);
                         }
                     },
                     err => {
-                        this.nerror('Get Branch Failure', err);
+                        let errInfo = '';
+                        try {
+                            errInfo = err.response.data['message'];
+                        } catch (error) {
+                            errInfo = err;
+                        }
+                        this.nerror('Get Branch Failure', errInfo);
+                    });
+            },
+            file () {
+                this.axios.defaults.withCredentials = true; // 带着cookie
+                this.axios.get('http://192.168.44.128:5000/saltshaker/api/v1.0/gitlab/file?product_id=' + this.productId + '&project_type=state_project&path=/&branch=' + this.branchName).then(
+                    res => {
+                        if (res.data['status'] === true) {
+                            this.fileTree = res.data['files']['file'];
+                            // this.branchName = this.branchData[0];
+                        } else {
+                            this.nerror('Get File Tree Failure', res.data['message']);
+                        }
+                    },
+                    err => {
+                        let errInfo = '';
+                        try {
+                            errInfo = err.response.data['message'];
+                        } catch (error) {
+                            errInfo = err;
+                        }
+                        this.nerror('Get File Tree Failure', errInfo);
                     });
             },
             loadData (item, callback) {
