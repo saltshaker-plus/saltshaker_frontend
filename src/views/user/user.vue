8fc94e6ad21d3cd8b6d8a16b41dd4e98101945e6
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-05-11 12:19:47 +0800

    Added user page

--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -2,7 +2,7 @@
     <div>
         <common-table :cColumns="cColumns" :apiService="apiService" :productShow="false" ref="childrenMethods">
             <Button slot="create" type="primary" @click="add('formValidate')">创建用户</Button>
-            <Modal slot="option" v-model="formView"  :title="optionTypeName">
+            <Modal slot="option" v-model="formView"  :title="optionTypeName" width="650px">
                 <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="60">
                     <FormItem label="用户名" prop="username">
                         <Input v-model="formValidate.username" placeholder="输入用户名"></Input>
@@ -10,31 +10,43 @@
                     <FormItem label="密码" prop="password">
                         <Input  type="password"　v-model="formValidate.password" placeholder="密码"></Input>
                     </FormItem>
-                    <FormItem label="产品线" prop="product">
-                        <CheckboxGroup v-model="formValidate.product" @on-change="handleSelect">
-                            <Checkbox v-for="item in userInfo.product" :key="item.id" :label="item.id">{{item.name}}</Checkbox>
-                        </CheckboxGroup>
-                    </FormItem>
-                    <FormItem label="组" prop="groups">
-                        <CheckboxGroup v-model="formValidate.groups">
-                             <Checkbox v-for="item in userInfo.groups" :key="item.id" :label="item.id">{{item.name}}</Checkbox>
-                        </CheckboxGroup>
-                    </FormItem>
                     <FormItem label="角色" prop="role">
                         <CheckboxGroup v-model="formValidate.role">
                             <Checkbox v-for="item in userInfo.role" :key="item.id" :label="item.id">{{item.name}}</Checkbox>
                         </CheckboxGroup>
                     </FormItem>
+                    <FormItem label="产品线" prop="product">
+                        <CheckboxGroup v-model="formValidate.product">
+                            <Checkbox v-for="item in userInfo.product" :key="item.id" :label="item.id" @click.prevent.native="handleCheckProduct(item.id)">{{item.name}}</Checkbox>
+                        </CheckboxGroup>
+                    </FormItem>
+                    <FormItem label="组" prop="groups">
+                        <Transfer
+                            :data="originGroup"
+                            :target-keys="targetGroup"
+                            :list-style="listStyleGroup"
+                            :render-format="rendersGroup"
+                            :titles = "titlesGroup"
+                            filterable
+                            @on-change="handleChangeGroup">
+                        </Transfer>
+                    </FormItem>
                     <FormItem label="ACL" prop="acl">
+                        <Transfer
+                            :data="originACL"
+                            :target-keys="targetACL"
+                            :list-style="listStyleACL"
+                            :render-format="rendersACL"
+                            :titles = "titlesACL"
+                            filterable
+                            @on-change="handleChangeACL">
+                        </Transfer>
                         <CheckboxGroup v-model="formValidate.acl">
                             <Checkbox v-for="item in userInfo.acl" :key="item.id" :label="item.id">{{item.name}}</Checkbox>
                         </CheckboxGroup>
                     </FormItem>
                 </Form>
                 <div slot="footer">
-                    {{formValidate.product}}
-                    {{formValidate.groups}}
-                    {{formValidate.acl}}
                     <Button type="ghost" @click="handleReset('formValidate')" style="margin-left: 8px">重置</Button>
                     <Button type="primary" @click="handleSubmit('formValidate')">提交</Button>
                 </div>
@@ -197,10 +209,16 @@
                                             this.optionTypeName = '编辑';
                                             this.id = params.row.id;
                                             this.formValidate.username = params.row.username;
-                                            this.formValidate.product = params.row.product.map(item => { return item.id; });
-                                            this.formValidate.groups = params.row.groups.map(item => { return item.id; });
+                                            this.originGroup = [];
+                                            this.originACL = [];
+                                            this.formValidate.product = params.row.product.map(item => {
+                                                this.getOriginGroup(item.id);
+                                                this.getOriginACL(item.id);
+                                                return item.id;
+                                            });
                                             this.formValidate.role = params.row.role.map(item => { return item.id; });
-                                            this.formValidate.acl = params.row.acl.map(item => { return item.id; });
+                                            this.targetGroup = params.row.groups.map(item => { return item.id; });
+                                            this.targetACL = params.row.acl.map(item => { return item.id; });
                                         }
                                     }
                                 }, '编辑'),
@@ -235,10 +253,7 @@
                 formValidate: {
                     username: '',
                     password: '',
-                    product: [],
-                    groups: [],
-                    role: [],
-                    acl: []
+                    product: []
                 },
                 ruleValidate: {
                     username: [
@@ -250,6 +265,21 @@
                     product: [
                         { required: true, type: 'array', min: 1, message: '至少选择一项', trigger: 'change' }
                     ]
+                },
+                // 穿梭框
+                originGroup: [],
+                targetGroup: [],
+                titlesGroup: ['待加分组', '当前分组'],
+                listStyleGroup: {
+                    width: '250px',
+                    height: '200px'
+                },
+                originACL: [],
+                targetACL: [],
+                titlesACL: ['待加ACL', '当前ACL'],
+                listStyleACL: {
+                    width: '250px',
+                    height: '200px'
                 }
             };
         },
@@ -272,8 +302,10 @@
                 this.optionType = 'add';
                 this.optionTypeName = '添加';
                 this.formView = true;
-                this.userInfo.groups = [];
-                this.userInfo.acl = [];
+                this.originGroup = [];
+                this.targetGroup = [];
+                this.originACL = [];
+                this.targetACL = [];
             },
             productList () {
                 this.axios.get(this.Global.serverSrc + 'product').then(
@@ -314,52 +346,21 @@
                         this.nerror('Get Role Failure', errInfo);
                     });
             },
-            aclList () {
-                this.axios.get(this.Global.serverSrc + 'acl?product_id=' + this.productId).then(
-                    res => {
-                        if (res.data['status'] === true) {
-                            this.userInfo.acl = res.data['data'];
-                        } else {
-                            this.nerror('Get ACL Failure', res.data['message']);
-                        }
-                    },
-                    err => {
-                        let errInfo = '';
-                        try {
-                            errInfo = err.response.data['message'];
-                        } catch (error) {
-                            errInfo = err;
-                        }
-                        this.nerror('Get ACL Failure', errInfo);
-                    });
-            },
-            groupList () {
-                this.axios.get(this.Global.serverSrc + 'groups?product_id=' + this.productId).then(
-                    res => {
-                        if (res.data['status'] === true) {
-                            this.userInfo.groups = res.data['data'];
-                        } else {
-                            this.nerror('Get Group Failure', res.data['message']);
-                        }
-                    },
-                    err => {
-                        let errInfo = '';
-                        try {
-                            errInfo = err.response.data['message'];
-                        } catch (error) {
-                            errInfo = err;
-                        }
-                        this.nerror('Get Group Failure', errInfo);
-                    });
-            },
             // 表单提
             handleSubmit (name) {
                 this.$refs[name].validate((valid) => {
                     if (valid) {
                         // 编辑
+                        let postData = {
+                            username: this.formValidate.username,
+                            password: this.formValidate.password,
+                            product: this.formValidate.product,
+                            groups: this.targetGroup,
+                            acl: this.targetACL
+                        };
                         if (this.optionType === 'edit') {
                             this.axios.put(this.Global.serverSrc + this.apiService + '/' + this.id,
-                                this.formValidate).then(
+                                postData).then(
                                 res => {
                                     if (res.data['status'] === true) {
                                         this.formView = false;
@@ -381,7 +382,7 @@
                         } else {
                             // 添加
                             this.axios.post(this.Global.serverSrc + this.apiService,
-                                this.formValidate).then(
+                                postData).then(
                                 res => {
                                     if (res.data['status'] === true) {
                                         this.formView = false;
@@ -410,18 +411,109 @@
             handleReset (name) {
                 this.$refs[name].resetFields();
             },
-            handleSelect (product) {
-                //this.formValidate.groups =
-                this.productId = product[0];
-                this.groupList();
-                this.aclList();
-                //this.userInfoAll.groups = [];
-//                for (var i = 0; i < product.length; i++) {
-//                    this.productId = product[i];
-//                    this.groupList();
-//                    console.log(this.userInfo.groups)
-//                    this.userInfoAll.groups = this.userInfo.groups
-//                }
+            // 穿梭框获取对应产品线的组信息
+            getOriginGroup (product) {
+                this.axios.get(this.Global.serverSrc + 'groups?product_id=' + product).then(
+                    res => {
+                        if (res.data['status'] === true) {
+                            for (var i = 0; i < res.data['data'].length; i++) {
+                                this.originGroup.push({
+                                    key: res.data['data'][i]['id'],
+                                    label: res.data['data'][i]['name'],
+                                    // 用于删除对应产品线的组
+                                    product: product
+                                });
+                            }
+                        } else {
+                            this.nerror('Get Info Failure', res.data['message']);
+                        };
+                    },
+                    err => {
+                        let errInfo = '';
+                        try {
+                            errInfo = err.response.data['message'];
+                            if (err.response.status === 404) {
+                                this.originGroup = [];
+                            } else {
+                                this.nerror('Get Info Failure', errInfo);
+                            }
+                        } catch (error) {
+                            errInfo = err;
+                            this.nerror('Get Info Failure', errInfo);
+                        }
+                    });
+            },
+            // 穿梭框获取对应产品线的ACL信息
+            getOriginACL (product) {
+                this.axios.get(this.Global.serverSrc + 'acl?product_id=' + product).then(
+                    res => {
+                        if (res.data['status'] === true) {
+                            for (var i = 0; i < res.data['data'].length; i++) {
+                                this.originACL.push({
+                                    key: res.data['data'][i]['id'],
+                                    label: res.data['data'][i]['name'],
+                                    product: product
+                                });
+                            }
+                        } else {
+                            this.nerror('Get ACL Failure', res.data['message']);
+                        }
+                    },
+                    err => {
+                        let errInfo = '';
+                        try {
+                            errInfo = err.response.data['message'];
+                        } catch (error) {
+                            errInfo = err;
+                        }
+                        this.nerror('Get ACL Failure', errInfo);
+                    });
+            },
+            // 处理穿梭框
+            handleChangeGroup (newTargetGroup) {
+                this.targetGroup = newTargetGroup;
+            },
+            rendersGroup (item) {
+                return item.label;
+            },
+            handleChangeACL (newTargetACL) {
+                this.targetACL = newTargetACL;
+            },
+            rendersACL (item) {
+                return item.label;
+            },
+            // 选择不同产品线时候,对应的组 ACL 跟着变化
+            handleCheckProduct (product) {
+                // 去除勾选
+                if (this.formValidate.product.includes(product)) {
+                    this.formValidate.product.splice(this.formValidate.product.indexOf(product), 1);
+                    // 组的操作
+                    for (let i = this.originGroup.length - 1; i >= 0; i--) {
+                        if (this.originGroup[i]['product'] === product) {
+                            // 右边组删除
+                            if (this.targetGroup.includes(this.originGroup[i]['key'])) {
+                                this.targetGroup.splice(this.targetGroup.indexOf(this.originGroup[i]['key']), 1);
+                            }
+                            // 左边组删除
+                            this.originGroup.splice(i, 1);
+                        }
+                    }
+                    // ACL的操作
+                    for (let i = this.originACL.length - 1; i >= 0; i--) {
+                        if (this.originACL[i]['product'] === product) {
+                            // 右边ACL删除
+                            if (this.targetACL.includes(this.originACL[i]['key'])) {
+                                this.targetACL.splice(this.targetACL.indexOf(this.originACL[i]['key']), 1);
+                            }
+                            // 左边ACL删除
+                            this.originACL.splice(i, 1);
+                        }
+                    }
+                } else {
+                    this.formValidate.product.push(product);
+                    this.getOriginGroup(product);
+                    this.getOriginACL(product);
+                }
             }
         }
     };
