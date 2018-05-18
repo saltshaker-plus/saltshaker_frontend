674b5e10c712218fb7137927f1783ccdb170cb07
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-05-18 08:08:22 +0800

    Deleted lock screen

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -31,7 +31,7 @@
                 </div>
                 <div class="header-avator-con">
                     <full-screen v-model="isFullScreen" @on-change="fullscreenChange"></full-screen>
-                    <lock-screen></lock-screen>
+                    <!--<lock-screen></lock-screen>-->
                     <message-tip v-model="mesCount"></message-tip>
                     <theme-switch></theme-switch>
                     
