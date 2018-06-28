b6c1fb6298492713609f0f9a9af2f2aeb576a4d7
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-06-28 19:58:40 +0800

    Delete unless router

--- src/router/router.js
+++ src/router/router.js
@@ -52,8 +52,6 @@ export const otherRouter = {
     children: [
         { path: 'home', title: {i18n: 'home'}, name: 'home_index', component: () => import('@/views/home/home.vue') },
         { path: 'ownspace', title: '个人中心', name: 'ownspace_index', component: () => import('@/views/own-space/own-space.vue') },
-        { path: 'order/:order_id', title: '订单详情', name: 'order-info', component: () => import('@/views/advanced-router/component/order-info.vue') }, // 用于展示动态路由
-        { path: 'shopping', title: '购物详情', name: 'shopping', component: () => import('@/views/advanced-router/component/shopping-info.vue') }, // 用于展示带参路由
         { path: 'task', title: '任务', name: 'task', component: () => import('@/views/job/task-info.vue') },
         { path: 'message', title: '消息中心', name: 'message_index', component: () => import('@/views/message/message.vue') }
     ]
@@ -171,84 +169,7 @@ export const appRouter = [
             { path: 'log', title: '操作日志', name: 'log', access: 103, component: () => import('@/views/audit_log/audit_log.vue') },
             { path: 'tools', title: '系统工具', name: 'tools', access: 104, component: () => import('@/views/error-page/error-page.vue') }
         ]
-    },
-    // {
-    //     path: '/component',
-    //     icon: 'social-buffer',
-    //     name: 'component',
-    //     title: '组件',
-    //     component: Main,
-    //     children: [
-    //         {
-    //             path: 'image-editor',
-    //             icon: 'crop',
-    //             name: 'image-editor',
-    //             title: '图片预览编辑',
-    //             component: () => import('@/views/my-components/image-editor/image-editor.vue')
-    //         },
-    //         {
-    //             path: 'draggable-list',
-    //             icon: 'arrow-move',
-    //             name: 'draggable-list',
-    //             title: '可拖拽列表',
-    //             component: () => import('@/views/my-components/draggable-list/draggable-list.vue')
-    //         },
-    //         {
-    //             path: 'file-upload',
-    //             icon: 'android-upload',
-    //             name: 'file-upload',
-    //             title: '文件上传',
-    //             component: () => import('@/views/my-components/file-upload/file-upload.vue')
-    //         },
-    //     ]
-    // },
-    // {
-    //     path: '/tables',
-    //     icon: 'ios-grid-view',
-    //     name: 'tables',
-    //     title: '表格',
-    //     component: Main,
-    //     children: [
-    //         { path: 'testTable', title: '翻页', name: 'test-table', icon: 'arrow-move', component: () => import('@/views/tables/test-table.vue') },
-    //         { path: 'dragableTable', title: '可拖拽排序', name: 'dragable-table', icon: 'arrow-move', component: () => import('@/views/tables/dragable-table.vue') },
-    //         { path: 'editableTable', title: '可编辑表格', name: 'editable-table', icon: 'edit', component: () => import('@/views/tables/editable-table.vue') },
-    //         { path: 'searchableTable', title: '可搜索表格', name: 'searchable-table', icon: 'search', component: () => import('@/views/tables/searchable-table.vue') },
-    //         { path: 'exportableTable', title: '表格导出数据', name: 'exportable-table', icon: 'code-download', component: () => import('@/views/tables/exportable-table.vue') },
-    //         { path: 'table2image', title: '表格转图片', name: 'table-to-image', icon: 'images', component: () => import('@/views/tables/table-to-image.vue') }
-    //     ]
-    // },
-    // {
-    //     path: '/advanced-router',
-    //     icon: 'ios-infinite',
-    //     name: 'advanced-router',
-    //     title: '高级路由',
-    //     component: Main,
-    //     children: [
-    //         { path: 'mutative-router', title: '动态路由', name: 'mutative-router', icon: 'link', component: () => import('@/views/advanced-router/mutative-router.vue') },
-    //         { path: 'argument-page', title: '带参页面', name: 'argument-page', icon: 'android-send', component: () => import('@/views/advanced-router/argument-page.vue') }
-    //     ]
-    // },
-    // {
-    //     path: '/access',
-    //     icon: 'key',
-    //     name: 'access',
-    //     title: '权限管理',
-    //     component: Main,
-    //     children: [
-    //         { path: 'index', title: '权限管理', name: 'access_index', component: () => import('@/views/access/access.vue') }
-    //     ]
-    // },
-    // {
-    //     path: '/access-test',
-    //     icon: 'lock-combination',
-    //     title: '权限测试页',
-    //     name: 'accesstest',
-    //     access: 0,
-    //     component: Main,
-    //     children: [
-    //         { path: 'index', title: '权限测试页', name: 'accesstest_index', access: 0, component: () => import('@/views/access/access-test.vue') }
-    //     ]
-    // }
+    }
 ];
 
 // 所有上面定义的路由都要写在下面的routers里
