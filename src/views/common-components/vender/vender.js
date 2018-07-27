382b27eb68acc09b95870673ddbbe22f4ccc745b
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-07-27 15:24:28 +0800

    删除：删除numPage.vue

--- src/views/common-components/vender/vender.js
+++ src/views/common-components/vender/vender.js
@@ -1,7 +1,12 @@
-export default function customError (title, info) {
+let vender = {};
+
+vender.nError = function (title, info) {
     this.$Notice.error({
         title: title,
-        desc: info,
+        // 替换<>避免被解析为html标签
+        desc: info.toString().replace(/<|>/g, ''),
         duration: 10
     });
-}
+};
+
+export default vender;
